8ea15ef36b232311f0b0f3da3917540a25bdefe4
MayaHaynesPrincess48 <166449349+MayaHaynesPrincess48@users.noreply.github.com>
2024-10-31 01:28:49 -0400

    add moreicon of lucide-react-icon

--- src/app/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET(): Promise<Response> {
-  return new Response(JSON.stringify({ status: 'up' }), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-}
